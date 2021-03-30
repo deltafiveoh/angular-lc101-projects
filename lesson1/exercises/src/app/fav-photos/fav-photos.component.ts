24174a2d9d3bc13a03f10fc4d8f245268c32b59d
deltafiveoh <75449438+deltafiveoh@users.noreply.github.com>
2021-03-29 19:49:09 -0500

    corrected exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images-na.ssl-images-amazon.com/images/I/51d12keDWpL._SX355_.jpg';
+  image3 = 'https://cdn.shopify.com/s/files/1/0939/2880/products/Yario_and_Moshi_Product_grande.jpg?v=1562631766';
 
   constructor() { }
 
